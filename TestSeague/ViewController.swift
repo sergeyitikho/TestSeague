cf94cdd7bb4e07167651da762d6c994dffea9fd0
John Doe <sergey@itikho.com>
2023-10-06 13:23:18 +0300

    Initial Commit

---
+++ TestSeague/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSeague
+//
+//  Created by John Doe on 06.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
